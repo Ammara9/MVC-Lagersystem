7c3ef9d60b2f4864e12d29004f71d124febe6345
Ammara Shakeel <ammarashakeel81@yahoo.se>
2024-10-23 16:57:45 +0200

    Add project files.

---
+++ Storage/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        //error:
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+    }
+}
